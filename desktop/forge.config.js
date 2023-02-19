04716e857b7da215bc3654f1ebadbae455769781
Michael Labieniec <michaellabieniec@gmail.com>
2023-02-19 16:11:36 -0500

    update config

--- desktop/forge.config.js
+++ desktop/forge.config.js
@@ -20,13 +20,6 @@ module.exports = {
     {
       name: '@electron-forge/maker-rpm',
       config: {},
-    },
-    {
-      name: '@electron-forge/maker-appx',
-      config: {
-        publisher: 'CN=716593F8-AD51-48ED-A55C-B92CD1EE76D3',
-        devCert: 'C:\Users\micha\AppData\Roaming\electron-windows-store\716593F8-AD51-48ED-A55C-B92CD1EE76D3\716593F8-AD51-48ED-A55C-B92CD1EE76D3.pfx',
-      }
     }
   ],
 };
